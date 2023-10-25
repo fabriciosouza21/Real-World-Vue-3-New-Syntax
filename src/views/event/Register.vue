f161893f6342dc7582fa3f9f68f2da286627e37b
Fabricio Souza <fabriciosouza21777@gmail.com>
2023-10-25 20:59:51 -0300

    programmatic navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,6 +1,16 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
 defineProps(['event'])
+const router = useRouter()
+const register = () => {
+  // If registered then redirect to event details
+  router.push({
+    name: 'EventDetails',
+  })
+}
 </script>
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me!</button>
 </template>
