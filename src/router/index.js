15f1df399cbc40cf61b4112aca30d3e2b6682f25
Fabricio Souza <fabriciosouza21777@gmail.com>
2023-10-25 20:50:47 -0300

    #redirect e alias

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const router = createRouter({
       props: (route) => ({ page: parseInt(route.query.page) || 1 }),
     },
     {
-      path: '/event/:id',
+      path: '/events/:id',
       name: 'EventLayout',
       props: true,
       component: EventLayout,
@@ -43,6 +43,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/event/:afterEvent(.*)',
+      redirect: (to) => {
+        return { path: '/events/' + to.params.afterEvent }
+      },
     },
   ],
 })
