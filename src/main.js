e7398511a72836f47654fb34ac309db9d59184d7
Fabricio Souza <fabriciosouza21777@gmail.com>
2023-10-26 09:07:52 -0300

    #Global Storage & Provide / Inject

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+const GStore = reactive({ flashMessage: '' })
+app.provide('GStore', GStore)
 app.use(createPinia())
 app.use(router)
 
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,12 +1,18 @@
 <script setup>
+import {inject} from 'vue';
 import { useRouter } from 'vue-router'
 
-defineProps(['event'])
+const { event } = defineProps(['event'])
+const GStore = inject('GStore')
 const router = useRouter()
 const register = () => {
   // If registered then redirect to event details
+  GStore.flashMessage = 'You are successfully registered for ' + event.title
+  setTimeout(() => {
+    GStore.flashMessage = ''
+  }, 3000)
   router.push({
-    name: 'EventDetails',
+    name: 'EventDetails'
   })
 }
 </script>
