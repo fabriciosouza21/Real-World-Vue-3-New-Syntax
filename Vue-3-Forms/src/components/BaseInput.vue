32f625d14ff1903e6ab8adcd68100c95635ef446
Fabricio Souza <fabriciosouza21777@gmail.com>
2023-12-08 17:21:00 -0300

    #id input unique

--- Vue-3-Forms/src/components/BaseInput.vue
+++ Vue-3-Forms/src/components/BaseInput.vue
@@ -7,10 +7,12 @@
     class="field"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
+    :id="uuid"
   >
 </template>
 
 <script>
+import UniqueID from '../services/UniqueID'
 export default {
   props: {
     label: {
@@ -20,6 +22,12 @@ export default {
     modelValue: {
       type: [String, Number],
       default: ''
+    }
+  },
+  setup () {
+    const uuid = UniqueID().getID()
+    return {
+      uuid
     }
   }
 
