cd3988306974872834cc88b7c021d3c29a5cb125
Fabricio Souza <fabriciosouza21777@gmail.com>
2023-12-08 17:18:23 -0300

    #fiedset

--- Vue-3-Forms/src/views/SimpleForm.vue
+++ Vue-3-Forms/src/views/SimpleForm.vue
@@ -1,33 +1,35 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form @submit.prevent="sendForm" >
+    <form @submit.prevent="sendForm">
 
       <BaseSelect v-model="event.category" label="Category" :options="categories">
       </BaseSelect>
 
-      <h3>Name & describe your event</h3>
+      <fieldset>
+        <legend>Name & describe your event</legend>
 
-      <BaseInput v-model="event.title" label="Title" type="text" class="base-input__input" />
+        <BaseInput v-model="event.title" label="Title" type="text" class="base-input__input" />
 
-      <BaseInput v-model="event.description" label="Description" type="text" class="base-input__input" />
+        <BaseInput v-model="event.description" label="Description" type="text" class="base-input__input" />
 
-      <h3>Where is your event?</h3>
+      </fieldset>
+      <fieldset>
+        <legend>Where is your event?</legend>
 
-      <BaseInput v-model="event.location" label="Location" type="text" class="base-input__input" />
+        <BaseInput v-model="event.location" label="Location" type="text" class="base-input__input" />
 
-      <h3>Are pets allowed?</h3>
-      <BaseRadioGroup
-        v-model="event.pets"
-        name="pets"
-        :options="petOptions"
-        />
+      </fieldset>
+      <fieldset>
+        <legend>Are pets allowed?</legend>
+        <BaseRadioGroup v-model="event.pets" name="pets" :options="petOptions" />
+      </fieldset>
 
-      <h3>Extras</h3>
-      <div>
-        <BaseCheckbox v-model="event.extras.catering" label="Catering">
+      <fieldset>
+        <legend>Extras</legend>
+        <BaseCheckbox v-model="event.extras.music" label="Music">
         </BaseCheckbox>
-      </div>
+      </fieldset>
 
       <pre>
         {{ event }}
@@ -89,3 +91,16 @@ export default {
   components: { BaseInput, BaseSelect, BaseCheckbox, BaseRadioGroup }
 }
 </script>
+<style>
+fieldset {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+
+legend {
+  font-size: 28px;
+  font-weight: 700;
+  margin-top: 20px;
+}
+</style>
