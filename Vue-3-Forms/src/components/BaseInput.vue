6b4d3a271cb4d38f39a21b07fa8b538b618e6e13
Fabricio Souza <fabriciosouza21777@gmail.com>
2023-12-08 17:37:49 -0300

    #assecibilidade

--- Vue-3-Forms/src/components/BaseInput.vue
+++ Vue-3-Forms/src/components/BaseInput.vue
@@ -8,7 +8,17 @@
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
     :id="uuid"
+    :aria-describedby="error ? `${uuid}-error` : null"
+    :aria-invalid="error ? true : null"
   >
+  <p
+    v-if="error"
+    class="errorMessage"
+    :id="`${uuid}-error`"
+    aria-live="assertive"
+  >
+    {{ error }}
+  </p>
 </template>
 
 <script>
@@ -21,6 +31,10 @@ export default {
     },
     modelValue: {
       type: [String, Number],
+      default: ''
+    },
+    error: {
+      type: String,
       default: ''
     }
   },
