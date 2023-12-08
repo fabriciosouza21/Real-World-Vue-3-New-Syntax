422708ae9764b2d185e9092a2833e5d84606d3ac
Fabricio Souza <fabriciosouza21777@gmail.com>
2023-12-08 12:10:02 -0300

    base input

---
+++ Vue-3-Forms/src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <label v-if="label" >{{ label }}</label>
+  <input
+    v-bind="$attrs"
+    type="text"
+    :placeholder="label"
+    class="field"
+    :value="modelValue"
+    @input="$emit('update:value', $event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+
+}
+</script>
