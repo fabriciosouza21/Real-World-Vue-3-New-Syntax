37078169792085f3d744e7b2d6cc1f7f088cf2b8
Fabricio Souza <fabriciosouza21777@gmail.com>
2024-01-01 09:34:10 -0300

    start whith variables reactive

---
+++ vue-3-typescript/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onMounted, reactive, ref } from 'vue'
+import fetchCount from './fetchCount'
+interface AppInfo {
+  name: string
+  slogan: string
+}
+
+const count = ref<number | null>(null)
+
+const appInfo = reactive({
+  name: 'Counter',
+  slogan: 'an app you can count on'
+})
+
+
+onMounted(() => {
+	// mock de exemplo
+  fetchCount((initialCount) => {
+    count.value = initialCount
+  })
+})
+
+</script>
+
+<template>
+	<div>
+	  <h1>{{ appInfo.name }}</h1>
+	  <h2>{{ appInfo.slogan }}</h2>
+	</div>
+	<p>{{ count }}</p>
+  </template>
